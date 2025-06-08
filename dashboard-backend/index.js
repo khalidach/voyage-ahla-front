da752c431f1e319f3489717b64ab10b662eb300b
khalidach <khalidplaysg@gmail.com>
2025-06-08 17:02:42 +0100

    fix problem

--- dashboard-backend/index.js
+++ dashboard-backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
-const cookieParser = require("cookie-parser"); // Add this line
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,8 +16,13 @@ app.use(
   })
 );
 app.use(express.json({ limit: "50mb" }));
-app.use(fileUpload({ useTempFiles: true }));
-app.use(cookieParser()); // Add this line to enable cookie parsing
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/", // Add this line to specify the temporary directory
+  })
+);
+app.use(cookieParser());
 
 // --- Database Connection ---
 const uri = process.env.MONGODB_URI;
